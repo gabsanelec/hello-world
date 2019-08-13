c3c756787ab47ac800b0bfefebd12a164cbe3908
gabsanelec <51341839+gabsanelec@users.noreply.github.com>
2019-08-13 14:49:51 -0500

    Ventanas en python
    Ventanas en python

---
+++ hello-git/ventanas.py
@@ -0,0 +1,13 @@
+'''
+Created on Aug 13, 2019
+
+@author: gabriel.santamaria
+'''
+from tkinter import *
+
+#Propiedades de ventana
+raiz = Tk()
+raiz.title("Ventana")
+raiz.resizable(1,1)
+#raiz.config(bg="yellow")
+raiz = mainloop()
